8ea250ca99dd24d2ae12d89e106a1ee8b255b87b
Elijah Soladoye <shodown96@gmail.com>
2024-12-15 17:35:30 +0100

    fix: partner change and removed excess screens

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+// const poppins = Poppins({ weight: "400" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
